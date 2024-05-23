b249a59aa51032dbb13d4859df927dec1c579a18
HanJiantong <jiantonghan@hotmail.com>
2024-05-23 20:25:29 +0800

    Update Settings.cs

--- Snake_by_JtH/Settings.cs
+++ Snake_by_JtH/Settings.cs
@@ -8,33 +8,34 @@ namespace Snake_by_JtH
     {
         static void ColorSettings()
         {
-            while (true)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                Console.Clear();
-                Console.WriteLine("设置/颜色设置");
-                Console.WriteLine("------------------------------------------------------------------------------------------------------");
-                Console.WriteLine("|     1 = 黑色       9 = 灰色                                                                        |");
-                Console.WriteLine("|     2 = 蓝色       A = 淡蓝色                                                                      |");
-                Console.WriteLine("|     3 = 绿色       B = 淡绿色                                                                      |");
-                Console.WriteLine("|     4 = 浅绿色     C = 淡浅绿色                                                                    |");
-                Console.WriteLine("|     5 = 红色       D = 淡红色                                                                      |");
-                Console.WriteLine("|     6 = 紫色       E = 淡紫色                                                                      |");
-                Console.WriteLine("|     7 = 黄色       F = 淡黄色                                                                      |");
-                Console.WriteLine("|     8 = 白色       G = 亮白色                                                                      |");
-                Console.WriteLine("|                                                                                                    |");
-                Console.WriteLine("| 0.返回                                                                                             |");
-
-                for (Int32 i = 0; i < 15; ++i)
+                while (true)
                 {
+                    Console.Clear();
+                    Console.WriteLine("设置/颜色设置");
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine("|     1 = 黑色       9 = 灰色                                                                        |");
+                    Console.WriteLine("|     2 = 蓝色       A = 淡蓝色                                                                      |");
+                    Console.WriteLine("|     3 = 绿色       B = 淡绿色                                                                      |");
+                    Console.WriteLine("|     4 = 浅绿色     C = 淡浅绿色                                                                    |");
+                    Console.WriteLine("|     5 = 红色       D = 淡红色                                                                      |");
+                    Console.WriteLine("|     6 = 紫色       E = 淡紫色                                                                      |");
+                    Console.WriteLine("|     7 = 黄色       F = 淡黄色                                                                      |");
+                    Console.WriteLine("|     8 = 白色       G = 亮白色                                                                      |");
                     Console.WriteLine("|                                                                                                    |");
-                }
+                    Console.WriteLine("| 0.返回                                                                                             |");
 
-                Console.WriteLine("------------------------------------------------------------------------------------------------------");
-                Console.WriteLine("按下对应键以选择");
-                Char Key = (Char)Console.ReadKey(true).Key;
+                    for (Int32 i = 0; i < 15; ++i)
+                    {
+                        Console.WriteLine("|                                                                                                    |");
+                    }
 
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine("按下对应键以选择");
+                    Char Key = (Char)Console.ReadKey(true).Key;
+
+
                     if (Key == '1')
                     {
                         Process.Start("cmd.exe", "/c color 07");
@@ -100,78 +101,41 @@ namespace Snake_by_JtH
                         Process.Start("cmd.exe", "/c color F0");
                     }
                 }
-                else
-                {
-                    if (Key == '1')
-                    {
-                        Console.Write("\033[37m\033[40m");
-}
-                    else if (Key == '2')
-                    {
-                        Console.Write("\033[37m\033[44m");
-                    }
-                    else if (Key == '3')
-                    {
-                        Console.Write("\033[37m\033[42m");
-                    }
-                    else if (Key == '4')
-                    {
-                        Console.Write("\033[30m\033[102m");
-                    }
-                    else if (Key == '5')
-                    {
-                        Console.Write("\033[37m\033[41m");
-                    }
-                    else if (Key == '6')
-                    {
-                        Console.Write("\033[37m\033[45m");
-                    }
-                    else if (Key == '7')
-                    {
-                        Console.Write("\033[30m\033[103m");
-                    }
-                    else if (Key == '8')
-                    {
-                        Console.Write("\033[30m\033[107m");
-                    }
-                    else if (Key == '9')
-                    {
-                        Console.Write("\033[37m\033[37m");
-                    }
-                    else if (Key == 'A' || Key == 'a')
-                    {
-                        Console.Write("\033[37m\033[104m");
-                    }
-                    else if (Key == 'B' || Key == 'b')
-                    {
-                        Console.Write("\033[37m\033[46m");
-                    }
-                    else if (Key == 'C' || Key == 'c')
-                    {
-                        Console.Write("\033[37m\033[106m");
-                    }
-                    else if (Key == 'D' || Key == 'd')
-                    {
-                        Console.Write("\033[37m\033[101m");
-                    }
-                    else if (Key == 'E' || Key == 'e')
-                    {
-                        Console.Write("\033[37m\033[105m");
-                    }
-                    else if (Key == 'F' || Key == 'f')
-                    {
-                        Console.Write("\033[37m\033[102m");
-                    }
-                    else if (Key == 'G' || Key == 'g')
-                    {
-                        Console.Write("\033[30m\033[107m");
-                    }
-                }
-
-                if (Key == '0')
-                {
-                    break;
-                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("设置/颜色设置");
+                Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|     错误：本功能暂不支持非Windows系统                                                                 |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("|                                                                                                    |");
+                Console.WriteLine("------------------------------------------------------------------------------------------------------");
+                Console.Write("请按任意键继续. . .");
+                Console.ReadKey();
+                return;
             }
 
             return;
@@ -211,6 +175,14 @@ namespace Snake_by_JtH
             Console.Write("请按任意键继续. . .");
             Console.ReadKey();
             return;
+        }
+
+        static void SpeedSettings()
+        {
+            while (true)
+            {
+                
+            }
         }
 
         public static void MainSettings()
